44beca045c689045ca1e46b95fe2da25b80aab13
IvanMedvedev11 <medvivan11@gmail.com>
2024-10-19 16:42:45 +0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,14 +1,16 @@
 def convert(currency, course):
     equivalent = currency / course
     return equivalent
-try:
-    currency = float(input("Введите кол-во валюты: "))
-    course = float(input("Введите курс: "))
-    if currency < 0:
-        raise Exception("Число должно быть не меньше 0")
-    elif course <= 0:
-        raise ZeroDivisionError("Курс должен быть больше нуля")
-except (ValueError, Exception, ZeroDivisionError):
-    print("Полечи голову, если денег хватит")
-else:
-    print(f"Результат: {convert(currency, course)}")
+def menu():
+    try:
+        currency = float(input("Введите кол-во валюты: "))
+        course = float(input("Введите курс: "))
+        if currency < 0:
+            raise Exception("Число должно быть не меньше 0")
+        elif course <= 0:
+            raise ZeroDivisionError("Курс должен быть больше нуля")
+    except (ValueError, Exception, ZeroDivisionError):
+        print("Полечи голову, если денег хватит")
+    else:
+        print(f"Результат: {convert(currency, course)}")
+menu()
